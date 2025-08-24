408d89c659e5ac6bd4149dbc3c66e4d547b0a7d7
Elias Velazquez <eliasvelazquez@vordentech.com>
2025-08-24 20:10:39 -0300

    feat: Add Sanity integration
    This commit integrates Sanity as the new CMS for the blog, replacing Wagtail.
    
    In oder to achieve this:
    - The sanity/astro integration is added
    - A configuration for sanity is added to astro.config.mjs
    - Both index files (src/pages/index.astro & src/pages/posts/index.astro) are updated with this integration by fetching content from SanityAPI using GROQ.
    - The PostDescription component no longer relies on the Fragment element, it can show the postDescription prop as is. In addition to this, it shows the postDate prop using toLocaleDateString().

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,21 @@ import tailwind from "@astrojs/tailwind";
 
 import netlify from "@astrojs/netlify/functions";
 
+import sanity from "@sanity/astro";
+
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind()],
-  output: "server",
-  adapter: netlify()
-});
+export default defineConfig(
+  {
+    integrations: [
+      tailwind(), 
+      sanity({
+      projectId: "y9uq1het",
+      dataset: "production",
+      useCdn: false,
+      apiVersion: '2025-08-24',
+      })
+    ],
+    output: "server",
+    adapter: netlify()
+  }
+);
